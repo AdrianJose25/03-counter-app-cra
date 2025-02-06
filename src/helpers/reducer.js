c6599246a5d4fa150e2b584536a3ed86739e8d0d
Adrian Colmenares <adriancolmenares15@gmail.com>
2025-02-05 21:52:25 -0400

    Reducer Mejorado

--- src/helpers/reducer.js
+++ src/helpers/reducer.js
@@ -6,46 +6,87 @@ export const initialState = {
   loading: false,
 };
 
-export function reducer(state, action) {
-  if (action.type === "START") {
-    return { ...state, approved: false, denied: false, dataErr: false };
-  } else if (action.type === "APPROVED") {
-    return {
-      ...state,
-      approved: true,
-      denied: false,
-      loading: false,
-    };
-  } else if (action.type === "DATAERR") {
-    return {
-      ...state,
-      approved: false,
-      denied: false,
-      dataErr: true,
-      loading: false,
-    };
-  } else if (action.type === "DENIED") {
-    return {
-      ...state,
-      approved: false,
-      denied: true,
-      loading: false,
-    };
-  } else if (action.type === "ONCHANGE") {
-    return {
-      ...state,
-      value: action.payload,
-      approved: false,
-      denied: false,
-      dataErr: false,
-      loading: false,
-    };
-  } else if (action.type === "ONCLICK") {
-    return {
-      ...state,
-      loading: true,
-    };
-  } else {
-    return { ...state };
+// export function reducer(state, action) {
+//   if (action.type === "START") {
+//     return { ...state, approved: false, denied: false, dataErr: false };
+//   } else if (action.type === "APPROVED") {
+//     return {
+//       ...state,
+//       approved: true,
+//       denied: false,
+//       loading: false,
+//     };
+//   } else if (action.type === "DATAERR") {
+//     return {
+//       ...state,
+//       approved: false,
+//       denied: false,
+//       dataErr: true,
+//       loading: false,
+//     };
+//   } else if (action.type === "DENIED") {
+//     return {
+//       ...state,
+//       approved: false,
+//       denied: true,
+//       loading: false,
+//     };
+//   } else if (action.type === "ONCHANGE") {
+//     return {
+//       ...state,
+//       value: action.payload,
+//       approved: false,
+//       denied: false,
+//       dataErr: false,
+//       loading: false,
+//     };
+//   } else if (action.type === "ONCLICK") {
+//     return {
+//       ...state,
+//       loading: true,
+//     };
+//   } else {
+//     return { ...state };
+//   }
+// }
+
+const objectReducer = (state, payload) => ({
+  START: { ...state, approved: false, denied: false, dataErr: false },
+  APPROVED: {
+    ...state,
+    approved: true,
+    denied: false,
+    loading: false,
+  },
+  DATAERR: {
+    ...state,
+    approved: false,
+    denied: false,
+    dataErr: true,
+    loading: false,
+  },
+  DENIED: {
+    ...state,
+    approved: false,
+    denied: true,
+    loading: false,
+  },
+  ONCHANGE: {
+    ...state,
+    value: payload,
+    approved: false,
+    denied: false,
+    dataErr: false,
+    loading: false,
+  },
+  ONCLICK: {
+    ...state,
+    loading: true,
+  },
+});
+
+export const reducer = (state, action) => {
+  if (objectReducer(state)[action.type]) {
+    return objectReducer(state, action.payload)[action.type];
   }
-}
+};
